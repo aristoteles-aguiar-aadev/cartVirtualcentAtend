95f52838b37b599360289c3c026d886863a5d60b
aristoteles-aguiar-aadev <aristotelesouza@gmail.com>
2023-01-10 01:25:29 -0300

    cartVirtualcentAtendimento

---
+++ cartVirtualFld/src/pages/IndexPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Unimed Ceara logo"
+      src="https://www.unimedceara.com.br/wp-content/themes/unimed/images/logo.png"
+      style="width: 180px; height: 108px"
+    >
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'IndexPage',
+});
+</script>
